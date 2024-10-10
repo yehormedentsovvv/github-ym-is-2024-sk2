a560e2a32b50cf1d95c018dc3e0d16ea11009d6e
Yehor Medentsov <yehor.medentsov@tul.cz>
2024-10-10 13:32:53 +0200

    changes 10.10

---
+++ IS Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {   
+        // chci aby se program opakoval po stisku klavesy "a"
+        string again = "a";
+
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("*********");
+            Console.WriteLine("*********** Výpis řady ************");
+            Console.WriteLine("****************");
+            Console.WriteLine("*********** Yehor Medentsov ************");
+            Console.WriteLine("******************************\n\n");
+            Console.WriteLine();
+
+            // Vstup do uživatele - TO-DO
+
+            // Logika pro výpis řady - TO-DO
+
+            // Opakování programu
+            
+            Console.WriteLine("Pro opakování programu stisknete klávesu 'a'");
+            again = Console.ReadLine();
+            
+                
+            
+
+
+
+
+        }
+    }
+}
