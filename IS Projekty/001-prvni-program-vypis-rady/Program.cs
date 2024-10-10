c8fb33ab6b27185f4daa7cf788ae6b568301c197
Yehor Medentsov <yehor.medentsov@tul.cz>
2024-10-10 13:53:22 +0200

    changes 10,10

--- IS Projekty/001-prvni-program-vypis-rady/Program.cs
+++ IS Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -17,7 +17,46 @@ class Program
             Console.WriteLine("******************************\n\n");
             Console.WriteLine();
 
-            // Vstup do uživatele - TO-DO
+            // Vstup do uživatele -  spatna varianta
+            //Console.Write("Zadejte prvni cislo rady: ");
+            //int first = int.Parse(Console.ReadLine());
+
+
+            // Vstup od uživatele -  lepsi varianta
+            Console.Write("Zadejte prvni cislo rady (cele cislo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first ))
+            {
+                Console.Write("Nezadali jste cele cislo. Zadejte prvni cislo rady (cele cislo): ");
+            }
+
+            Console.Write("Zadejte posledni cislo rad (cele cislo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last ))
+            {
+                Console.Write("Nezadali jste cele cislo. Zadejte posledni cislo rady (cele cislo): ");
+            }
+
+            Console.Write("Zadejte diference (cele cislo): ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step ))
+            {
+                Console.Write("Nezadali jste cele cislo. Zadejte diference (cele cislo): ");
+            }
+
+
+
+            //Vypis uzivatelskeho vstupu
+            Console.WriteLine();
+            Console.WriteLine("================");
+            Console.WriteLine("Prvni cislo: {0}", first);
+            Console.WriteLine("Posledni cislo: {0}", last);
+            Console.WriteLine("Diference cislo: {0}", step);
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
 
             // Logika pro výpis řady - TO-DO
 
@@ -25,13 +64,6 @@ class Program
             
             Console.WriteLine("Pro opakování programu stisknete klávesu 'a'");
             again = Console.ReadLine();
-            
-                
-            
-
-
-
-
         }
     }
 }
