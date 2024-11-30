d774db0359df191d88061349edc57cccaa6fb748
Yehor Medentsov <yehor.medentsov@tul.cz>
2024-11-30 17:33:49 +0100

    + home #14

--- IS Projekty/program014a-10to2/Program.cs
+++ IS Projekty/program014a-10to2/Program.cs
@@ -16,29 +16,64 @@ while(again == "a") {
         Console.Write("Špatný vstup. Zadejte číslo v desítkové soustavě (přirozené číslo): ");
     }
 
+    Console.Write("Do jaké číselné soustavy chcete číslo převést? (1 - 2-ova; 2 -  5-ova; 3 - 8-ova : ");
+    int choice;
+    while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 6) { 
+    Console.Write("Nezadali jste číslo 1-3. Zadejte znovu: ");
+    }
+    Console.WriteLine("Vybrali jste možnost: " + choice);
     uint[] myArray = new uint[32];
     uint zaloha = cislo;
     uint zbytek;
-
     uint i=0;
-    while(cislo > 0) {
+
+    if(choice == 1)
+    {
+        while(cislo > 0) {
         zbytek = cislo % 2;
         cislo = (cislo - zbytek)/2;
         myArray[i] = zbytek;
-        
         Console.ForegroundColor = ConsoleColor.DarkGreen;
         Console.WriteLine("Celá část = {0}; zbytek = {1}", cislo, zbytek);
-        
         i++;
+        }
+        Console.WriteLine("Poslední využitý index pole: {0}", i-1);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\n\nVýsledek: ");
+    }
+    else if(choice == 2)
+    {
+        while(cislo > 0) {
+        zbytek = cislo % 5;
+        cislo = (cislo - zbytek)/5;
+        myArray[i] = zbytek;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("Celá část = {0}; zbytek = {1}", cislo, zbytek);
+        i++;
+        }
+        Console.WriteLine("Poslední využitý index pole: {0}", i-1);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\n\nVýsledek: ");
+
+    }
+    else{
+        while(cislo > 0) {
+        zbytek = cislo % 8;
+        cislo = (cislo - zbytek)/8;
+        myArray[i] = zbytek;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("Celá část = {0}; zbytek = {1}", cislo, zbytek);
+        i++;
+        }
+        Console.WriteLine("Poslední využitý index pole: {0}", i-1);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\n\nVýsledek: ");
     }
 
-    Console.WriteLine("Poslední využitý index pole: {0}", i-1);
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine("\n\nVýsledek: ");
-
-    for(uint j=i-1; j>=0;j--) {
+    for(int j = (int)i - 1; j >= 0; j--) {
         Console.Write("{0}",myArray[j]);
     }
+    Console.WriteLine("\n\n");
 
     Console.ForegroundColor = ConsoleColor.White;
 
